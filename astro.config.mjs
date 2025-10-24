d32782bc9e2583aa2d38be1f9802e232e7a2b7dd
Eirik Kjeken <Eirik.Kjeken@sintef.no>
2025-10-24 18:02:24 +0200

    Route things to /dvd

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://kjekensintef.github.io',
-    base: '/dvd/',
+    base: '/dvd',
 });
