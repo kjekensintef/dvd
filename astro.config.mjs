cf0d3922b64a2b8614b7f2b3780c9a3ee6b01073
Eirik Kjeken <Eirik.Kjeken@sintef.no>
2025-10-27 16:36:37 +0100

    Use new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://kjekensintef.github.io',
-    base: '/dvd/',
+    site: 'https://dinvatedrom.no',
+    base: '/',
 });
